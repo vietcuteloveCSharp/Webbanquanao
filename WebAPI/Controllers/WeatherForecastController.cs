8c9fa30b976a3663129a3f578c56fd0c2dfbbb6c
LAPTOP-T78D6ENU\nhuhoang <nhuhoang2001524@gmail.com>
2024-10-16 17:31:51 +0700

    Settup cách gọi api từ phía backend và mẫu gọi backend api trong HomeController

--- WebAPI/Controllers/WeatherForecastController.cs
+++ WebAPI/Controllers/WeatherForecastController.cs
@@ -10,7 +10,12 @@ namespace WebAPI.Controllers
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
-
+        private static readonly IEnumerable<WeatherForecast> _forecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        }).ToList();
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
@@ -21,13 +26,12 @@ namespace WebAPI.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return _forecasts;
+        }
+        [HttpGet("GetWeatherForecastByTemp")]
+        public WeatherForecast? GetByTempecher(int temp)
+        {
+            return _forecasts.FirstOrDefault(x => x.TemperatureC == temp || x.TemperatureF == temp);
         }
     }
 }
---
+++ WebView/Extensions/GetHttpClient.cs
@@ -0,0 +1,29 @@
+﻿namespace WebView.Extensions
+{
+    public class GetHttpClient
+    {
+        private IHttpClientFactory httpClientFactory;
+        private const string HeaderKey = "Authorization";
+
+        public GetHttpClient(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        // Thêm token ở local store và thêm vào header của request
+        public HttpClient GetPrivateHttpClient()
+        {
+            // Mở kết nối dến Server có tên "SystemApiClient"
+            var client = httpClientFactory.CreateClient("SystemApiClient");
+            return client;
+        }
+
+        // Trả về Httpclient ko có chứa thông tin Authorization -> tăng tính bảo mật khi xóa token của user
+        public HttpClient GetPublichHttpClient()
+        {
+            var client = httpClientFactory.CreateClient("SystemApiClient");
+            client.DefaultRequestHeaders.Remove(HeaderKey);
+            return client;
+        }
+    }
+}
