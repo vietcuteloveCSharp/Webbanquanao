0d4a0a4fb43349dd5e83e3e41994f9331cdec71f
LAPTOP-T78D6ENU\nhuhoang <nhuhoang2001524@gmail.com>
2024-12-04 17:32:27 +0700

    Ban tai quay - 4/12

--- WebView/Areas/BanTaiQuay/Controllers/BanNhanhController.cs
+++ WebView/Areas/BanTaiQuay/Controllers/BanNhanhController.cs
@@ -1,4 +1,5 @@
 ﻿using DAL.Context;
+using DAL.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebView.Areas.BanTaiQuay.Controllers
@@ -15,6 +16,34 @@ namespace WebView.Areas.BanTaiQuay.Controllers
         {
 
             return View("BanNhanh");
+        }
+
+        // API lấy sản phẩm với tìm kiếm và phân trang
+        [HttpGet]
+        public IActionResult GetProducts(string searchQuery = "", int page = 1, int pageSize = 10)
+        {
+            var products = new List<SanPham>();
+
+            if (!string.IsNullOrEmpty(searchQuery))
+            {
+                products = _dbContext.SanPhams.ToList().Where(p => p.Ten.Contains(searchQuery.Trim(), StringComparison.OrdinalIgnoreCase)).ToList();
+            }
+
+            int totalProducts = products.Count();
+            int totalPages = (int)Math.Ceiling((double)totalProducts / pageSize);
+
+            var paginatedProducts = products
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToArray();
+
+            return Json(new
+            {
+                Products = paginatedProducts,
+                CurrentPage = page,
+                TotalPages = totalPages,
+                SearchQuery = searchQuery
+            });
         }
     }
 }
