cbf3f185972c667be8938eb474d0b128f744160c
thetuyen0802 <thetuyen0802@gmail.com>
2025-01-13 23:17:30 +0700

    Emun

--- Enum/EnumVVA/ETrangThaiHD.cs
+++ Enum/EnumVVA/ETrangThaiHD.cs
@@ -9,13 +9,14 @@ namespace Enum.EnumVVA
     public enum ETrangThaiHD
     {
         // Trạng thái chung
-        //ChoXuLy = 0,           // Hóa đơn đang chờ xử lý
-        HoanThanhDon = 5,      // Đơn hàng đã hoàn thành
-        HuyDon = 7,            // Đơn hàng bị hủy
+       
+      
         // Trạng thái dành riêng cho bán online
         ChoXacNhan = 1,        // Đơn hàng chờ xác nhận từ người bán
         ChoThanhToan = 2,      // Đơn hàng chờ thanh toán (nếu không phải COD)
         DaXacNhan = 3,     // Đơn hàng đang được vận chuyển (đã thanh toán trước)
-        DangVanChuyen = 4,  // Đơn hàng COD đang được vận chuyển
+        DangVanChuyen = 4,  // Đơn hàng  đang được vận chuyển
+        HoanThanhDon = 5,      // Đơn hàng đã hoàn thành
+        HuyDon = 6,            // Đơn hàng bị hủy
     }
 }
