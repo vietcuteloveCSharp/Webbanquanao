da9554e077153ad81d29a3962275cac1a4d82948
LAPTOP-T78D6ENU\nhuhoang <nhuhoang2001524@gmail.com>
2025-05-04 19:51:33 +0700

    update thanh toan

---
+++ WebView/Models/Vnpay/RefundRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace WebView.Models.Vnpay
+{
+    public class RefundRequest
+    {
+        public decimal Amount { get; set; }
+        public string TransactionDate { get; set; }
+        public string TransactionNo { get; set; }
+        public string TxnRef { get; set; }
+        public string CreateBy { get; set; } = "KhacHangShopCanMan";
+        public string TransactionType { get; set; }
+    }
+}
